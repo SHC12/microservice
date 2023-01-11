724aa91856f3690ab6be7794bca512716a2b0a3d
shc12 <superhumancrewid12@gmail.com>
2023-01-11 23:41:41 +0700

    chore: init repository

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPlugins = require("next-compose-plugins")
+const withCss = require("@zeit/next-css")
+const withImages = require("next-images")
+const withSvg = require("next-react-svg")
 
-module.exports = nextConfig
+const path = require("path")
+const { CONFIG_FILES } = require("next/dist/shared/lib/constants")
+
+module.exports = withPlugins(
+  [
+    withCss({}),
+    withImages({}),
+    withSvg({
+      include: path.resolve(__dirname, "public/images"),
+      webpack(config,options){
+        return config;
+      }
+    }),
+  ]
+)
